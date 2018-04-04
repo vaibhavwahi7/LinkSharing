ad924ac031ef51e3434192bcefc173f154ec00f8
vaibhav <vaibhav.wahi@gmail.com>
2018-04-05 03:12:13 +0530

    Question1

---
+++ grails-app/taglib/linksharing/Cust_TagLib.groovy
@@ -0,0 +1,10 @@
+package linksharing
+
+class Cust_TagLib {
+//    static defaultEncodeAs = [taglib:'html']
+    //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
+    static tags=[taglib:'mark']
+
+    static namespace='ls'
+
+}
