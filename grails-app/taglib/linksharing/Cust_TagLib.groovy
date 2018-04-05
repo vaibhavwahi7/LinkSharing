3a4fb915bf9120b38db5c08b184f5354aca86ce0
vaibhav <vaibhav.wahi@gmail.com>
2018-04-05 12:38:36 +0530

    Question2andQuestion3

--- grails-app/taglib/linksharing/Cust_TagLib.groovy
+++ grails-app/taglib/linksharing/Cust_TagLib.groovy
@@ -3,37 +3,32 @@ package linksharing
 class Cust_TagLib {
 //    static defaultEncodeAs = [taglib:'html']
     //static encodeAsForTags = [tagName: [taglib:'html'], otherTagName: [taglib:'none']]
-    static tags=[taglib:'mark']
+    static tags = [taglib: 'mark']
 
-    static namespace='ls'
-def mark ={attrs,body->
+    static namespace = 'ls'
+    def mark = { attrs, body ->
 
-    def isRead=attrs?.id
-    if(session.user!=null) {
-        render "tag" $ { isRead }
-        Integer id=session.user.id
-        User user =new User.get(id)
-        Resource resource=Resource.get(isRead)
-        ReadingItem readingItem=ReadingItem.findAllByResource(resource)
-        readingItem.each={
-            if(it.user==user)
-            {
-                out<< "<a href='#' >Read</a>"
+        def isRead = attrs?.id
+        if (session.user != null) {
+            render "tag" $ { isRead }
+            Integer id = session.user.id
+            User user = new User.get(id)
+            Resource resource = Resource.get(isRead)
+            ReadingItem readingItem = ReadingItem.findAllByResource(resource)
+            readingItem.each = {
+                if (it.user == user) {
+                    out << "<a href='#' >Read</a>"
+                } else {
+                    out << "<a href='#'>Unread</a>"
+                }
+
             }
-            else
-            {
-       out<< "<a href='#'>Unread</a>"
-            }
 
+
+        } else {
+            render "Session does not exist"
         }
 
-
     }
-    else
-{
-    render "Session does not exist"
-}
-
-}
 }
 
